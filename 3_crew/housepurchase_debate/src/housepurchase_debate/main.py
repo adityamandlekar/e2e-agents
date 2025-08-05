1d1d5b98da319b5c47514602fb49678461f2b7c4
Aditya Mandlekar <adityamandlekar@gmail.com>
2025-08-05 16:56:51 -0400

    House purchase crewai

---
+++ 3_crew/housepurchase_debate/src/housepurchase_debate/main.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+import sys
+import warnings
+
+from datetime import datetime
+
+from housepurchase_debate.crew import HousepurchaseDebate
+
+warnings.filterwarnings("ignore", category=SyntaxWarning, module="pysbd")
+
+def run():
+    """
+    Run the crew.
+    """
+    inputs = {
+        'motion': 'One should always rent a house for whole life instead of buying a house. The cost of renting a house is less than the cost of buying a house.',
+    }
+    
+    try:
+        result=HousepurchaseDebate().crew().kickoff(inputs=inputs)
+        print(result)
+    except Exception as e:
+        raise Exception(f"An error occurred while running the crew: {e}")
